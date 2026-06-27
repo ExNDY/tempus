51d01c52d67964e713ebb76e30dc324b756779be
Kolchurin Konstantin <kkolchurin@icerockdev.com>
2026-06-27 21:25:19 +0700

    Clean up DI module imports

--- app/src/main/java/com/cappielloantonio/tempo/di/appModule.kt
+++ app/src/main/java/com/cappielloantonio/tempo/di/appModule.kt
@@ -4,7 +4,6 @@ import androidx.media3.common.util.UnstableApi
 import com.cappielloantonio.tempo.database.AppDatabase
 import com.cappielloantonio.tempo.util.Preferences
 import org.koin.dsl.module
-import org.koin.android.ext.koin.androidContext
 
 @OptIn(UnstableApi::class)
 val appModule = module {
